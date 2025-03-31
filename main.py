eb8050f8b9bbaf74460d60f528dbbc2a76f44243
HERICK AKIO YOSHII KUMATA <154487912+haykCAKI@users.noreply.github.com>
2025-03-31 14:05:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import random
+
 app = FastAPI()
 
 
@@ -13,3 +15,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": True, "num_aleatorio": random.radiant(0, 100)}
